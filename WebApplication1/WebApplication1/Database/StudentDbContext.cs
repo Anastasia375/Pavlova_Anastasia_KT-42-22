3d576a86de74ee85f6b0f6140cfbd6a616d67584
Anastasia375 <asta1004@mail.ru>
2025-02-21 14:47:46 +0300

    Добавлены классы Student, Group.

---
+++ WebApplication1/WebApplication1/Database/StudentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore
+
+    namespace Pavlova_Anastasia_KT_42_22
+{
+    public class StudentDbcontext : DbContext
+    {
+        public StudentDbcontext(DbContextOptions<StudentDbcontext> options)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Pavlova_Anastasia_KT_42_22.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set set; }
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public int GroupId{ get; set; }
+}
